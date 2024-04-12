f8b0ad62e1af383236db45bc352400f69f979d9a
abbos-ismailov <abbosismailov0017@gmail.com>
2024-04-12 18:37:56 +0500

    wishlist is finished

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -1,3 +1,8 @@
+from django.urls import include, path
+from .views import WishListAddApiView, WishlistGetApiView
+
+
 urlpatterns = [
-    
+    path('add-wish/', WishListAddApiView.as_view()),
+    path('get-wish/', WishlistGetApiView.as_view()),
 ]
--- core/settings.py
+++ core/settings.py
@@ -127,6 +127,8 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 1  # Number of items per page
 }
 
 # Database
