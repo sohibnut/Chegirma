1155603901a75e37f37d3a75b61222fac8ce52a3
Abdulaziz <abdulazizmusurmonov204@gmail.com>
2024-04-12 21:33:55 +0500

    commit1

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -1,3 +1,10 @@
+from rest_framework.urls import path
+from .views import ProductCategoryview, ProductSellerView, SearchFilterView
+
+
 urlpatterns = [
+    path("category/<uuid:uuid>/", ProductCategoryview.as_view(), name='categoryfilter'),
+    path("seller/<uuid:uuid>/", ProductSellerView.as_view(), name='sellerfilter'),
+    path("search/", SearchFilterView.as_view())
     
 ]
--- apps/home/views.py
+++ apps/home/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from .serializer import ProductCategorySerialize
+from .models import Product
+from rest_framework.response import Response
+from rest_framework import filters
+
+
+class ProductCategoryview(APIView):
+    permission_classes = [AllowAny, ]
+    serializer_class = ProductCategorySerialize
+    
+    def get(self , request, uuid):
+         
+        querset = Product.objects.filter(category = uuid )# children is not pulli
+        serializer_data = self.serializer_class(querset, many = True)
+        return Response(serializer_data.data)
+    
+class ProductSellerView(APIView):
+    
+    permission_classes = [AllowAny]
+    serializer_class = ProductCategorySerialize
+    
+    def get(self, request, uuid):
+        
+        queryset = Product.objects.filter(seller = uuid)
+        serializer_data = self.serializer_class(queryset, many = True)
+        
+        return Response(serializer_data.data)
+    
+    
+class SearchFilterView(ListAPIView):
+    
+    permission_classes = (AllowAny, )
+    queryset = Product.objects.all()
+    serializer_class = ProductCategorySerialize
+    filter_backends = [filters.SearchFilter]  ####
+    search_fields = ['color__name']
