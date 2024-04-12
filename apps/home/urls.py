04259e3b362beb0f5b85cf66ef74e5ff84a16d95
Javlonbek Boriyev <153982223+Javlonbek3388@users.noreply.github.com>
2024-04-12 21:33:04 +0500

    first commit

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -1,3 +1,7 @@
+from rest_framework.urls import path
+from .views import ProductCategoryView, SellerView
+
 urlpatterns = [
-    
+    path("category/<uuid:uuid>/", ProductCategoryView.as_view()),
+    path("seller/<uuid:uuid>/", SellerView.as_view()),
 ]
--- apps/home/views.py
+++ apps/home/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializers import ProductCategorySerializer
+from .models import Product
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+
+class ProductCategoryView(APIView):
+    serializer_class = ProductCategorySerializer
+    permission_classes = [AllowAny]
+
+    def get(self, request, uuid):
+        queryset = Product.objects.filter(category=uuid)
+        serializer_data = self.serializer_class(queryset, many=True)
+        return Response(serializer_data.data)
+
+
+class SellerView(APIView):
+    serializer_class = ProductCategorySerializer
+    permission_classes = [AllowAny]
+
+    def get(self, request, uuid):
+        queryset = Product.objects.filter(seller=uuid)
+        serializer_data = self.serializer_class(queryset, many=True)
+        return Response(serializer_data.data)
+
