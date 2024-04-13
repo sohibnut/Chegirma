3508c9792b375afb00837a38cf2934e40812f512
Javlonbek Boriyev <153982223+Javlonbek3388@users.noreply.github.com>
2024-04-13 21:15:27 +0500

    price finished

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -1,8 +1,9 @@
 from rest_framework.urls import path
-from .views import ProductCategoryView, SellerView, ProductListView
+from .views import ProductCategoryView, SellerView, ProductListView, ProductPriceView
 
 urlpatterns = [
     path("category/<uuid:uuid>/", ProductCategoryView.as_view()),
     path("seller/<uuid:uuid>/", SellerView.as_view()),
     path('product/', ProductListView.as_view()),
+    path('price/', ProductPriceView.as_view()),
 ]
--- apps/home/views.py
+++ apps/home/views.py
@@ -43,3 +43,20 @@ class ProductListView(ListCreateAPIView):
             queryset = queryset.filter(color__uuid=color_uuid)
 
         return queryset
+
+
+class ProductPriceView(ListCreateAPIView):
+    serializer_class = ProductCategorySerializer
+    permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        queryset = Product.objects.all()
+        max_price = self.request.query_params.get('max_price')
+        min_price = self.request.query_params.get('min_price')
+
+        if min_price is not None:
+            queryset = queryset.filter(discount_price__gte=min_price)
+        if max_price is not None:
+            queryset = queryset.filter(discount_price__lte=max_price)
+
+        return queryset
