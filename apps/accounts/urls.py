14c5a5120a650553e1bd3953c1ff0baaa06854c6
s1rojiddinnov1c <s1rojiddinnov1c@gmail.com>
2024-04-15 17:55:10 +0500

    "t"

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,3 +1,27 @@
+from django.urls import path
+from .views import(SignUpApiView,
+                   VerifyCodeApiView,
+                   PersonalDataUpdadeApiView,
+                   LoginApiView,
+                   LogoutApiView
+                   )
+
+
+
+
+
+
+
+
+
+
 urlpatterns = [
-    
+    path('signup/',SignUpApiView.as_view()),
+    path('code_verify/', VerifyCodeApiView.as_view()),
+    path('personal_data/', PersonalDataUpdadeApiView.as_view()),
+    path('login/', LoginApiView.as_view()),
+    path('logout/', LogoutApiView.as_view()),
+
+
+
 ]
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView,APIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from .serializer import SignUpSerializer
+from .models import UserModel
+from rest_framework.response import Response
 
-# Create your views here.
+
+
+
+
+
+class SignUpApiView(CreateAPIView):
+    permission_classes = (AllowAny, )
+    queryset = UserModel.objects.all()
+    serializer_class = SignUpSerializer
+
+class VerifyCodeApiView(APIView):
+    permission_classes = (IsAuthenticated, )
+    def post(self, request, *args, **kwargs):
+        user = self.request.user
+        code = self.request.data.get('code')
+        self.check_code(user, code)
+        
+        data = {
+            'status' : True,
+            'auth_status' : user.auth_status,
+            'access' : user.token()['access'],
+            'refresh' : user.token()['refresh']
+        }
+        return Response(data)
