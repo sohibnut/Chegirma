5201cc7063c71cdc2b3b51f4cef42995126b477c
abbos-ismailov <abbosismailov0017@gmail.com>
2024-06-10 15:15:05 +0500

    wish-list

--- apps/home/views.py
+++ apps/home/views.py
@@ -7,9 +7,6 @@ from apps.accounts.models import UserModel
 from .models import WishlistItem
 from rest_framework.response import Response
 from apps.base.utility import CustomPagination
-# Create your views here.
-
-
 
 
 class WishListAddApiView(APIView):
@@ -17,18 +14,18 @@ class WishListAddApiView(APIView):
     serializer_class = WishListAddSerializer 
 
     def post(self, request, *args, **kwargs):
-        # user = get_object_or_404(UserModel, username=request.user)
-        # request.data["user"] = user.uuid
+        user = get_object_or_404(UserModel, username=request.user)
+        request.data["user"] = user.uuid
         data = request.data
         product = data.get("product")
         serializer = self.serializer_class(data=data)
-        ### delete wishlist
+        #TODO: delete wishlist
         check_cloth = WishlistItem.objects.filter(product=product).first()
         if check_cloth:
             check_cloth.delete()
             response_data = {"status": True, "message": f"Wish Listga o'chirildi {product}"}
             return Response(response_data)
-        ### create wishlist
+        #TODO: create wishlist
         elif serializer.is_valid(raise_exception=True):
             serializer.save()
             response_data = {"status": True, "message": f"Wish Listga qo'shildi {product}"}
@@ -45,21 +42,16 @@ class WishlistGetApiView(APIView):
             products = WishlistItem.objects.all().order_by("product")
             paginator = self.pagination_class()
             result_page = paginator.paginate_queryset(products, request)
-            
             # Get the requested page size from the query parameters
             page_size = request.query_params.get('page_size')
-
             # Set the page size in the paginator
             if page_size:
                 paginator.page_size = page_size
-
             result_page = paginator.paginate_queryset(products, request)
             serializer = WishListAddSerializer(result_page, many=True)
-
             # Get the next and previous page URLs
             next_page = paginator.get_next_link()
             previous_page = paginator.get_previous_link()
-
             count = len(serializer.data)
             data = {
                 "status": True,
