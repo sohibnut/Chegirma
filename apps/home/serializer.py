54b15e58cfd425e84a8daf7a568582b0ab858ba6
Sohibnut <mr.nut.sohib@gmail.com>
2024-06-29 16:27:03 +0500

    seller name and comments count added

--- apps/home/serializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Category, Product
-
-
-class ProductCategorySerialize(serializers.ModelSerializer):
-    
-    class Meta :
-        model = Product
-        fields = '__all__'
--- apps/home/serializers.py
+++ apps/home/serializers.py
@@ -7,9 +7,21 @@ class CommentSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class ProductSerializer(serializers.ModelSerializer):
+
+    seller_name = serializers.SerializerMethodField("get_seller_name")
+    comments_count = serializers.SerializerMethodField("get_comments_count")
+
     class Meta:
         model = Product
         fields = "__all__"
+
+    @staticmethod
+    def get_seller_name(obj):
+        return obj.seller.name
+
+    @staticmethod
+    def get_comments_count(obj):
+        return obj.product_comments.count()
         
 
 class CompareSerializer(serializers.ModelSerializer):
